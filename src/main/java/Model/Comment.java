27e2a2a554a2dbbc663177d3420c885abf408081
Redas Baltrenas <reduxas02@gmail.com>
2026-05-01 13:57:04 +0300

    Model and Repository

--- src/main/java/Model/Comment.java
+++ src/main/java/Model/Comment.java
@@ -1,4 +1,38 @@
 package Model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+@Entity
 public class Comment {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String comment;
+    private LocalDateTime date;
+    private Boolean status;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
